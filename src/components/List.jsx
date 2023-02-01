b6e7e12858755b73e5858ab2e3033b409d0bcd55
viniciosragazzi <viniciosragazzzi@gmail.com>
2023-01-31 22:08:11 -0300

    Commit

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,22 +1,113 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
 
+// import required modules
+import { Navigation } from "swiper";
 export function List({ type }) {
   const typeName = location.pathname.split("/");
-  const toCamelCase = (stg) => {
-    let str = stg[2].split("");
-    let palavraCerta = "";
-    let p = "";
-    const palavra = str.forEach((l, i) => {
-      if (i != 0) {
-        p += l;
-      }
-    });
-    return (palavraCerta = str[0].toUpperCase() + p);
-  };
-  toCamelCase(typeName);
+  let str = typeName[2] ? typeName[2].split("") : typeName[1].split("");
   return (
-    <div>
-      <h1 className="text-xl text-white">{toCamelCase(typeName)}</h1>
+    <div className="flex flex-col gap-14">
+      <div className="listSlide h-52 mt-10">
+        <h1 className="text-white text-xl font-semibold mb-4">Lançamentos</h1>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+          }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="listSlide h-52 mt-10">
+        <h1 className="text-white text-xl font-semibold mb-4">
+          Ação / Aventura
+        </h1>
+
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+          }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="listSlide h-52 mt-10">
+        <h1 className="text-white text-xl font-semibold mb-4">Comédia</h1>
+
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+          }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,50 @@
-import React, { useEffect , useState } from "react";
-import {useLocation, NavLink,useMatch } from "react-router-dom";
+import { Popcorn } from "phosphor-react";
+import React, { useEffect, useState } from "react";
+import { useLocation, NavLink, useMatch } from "react-router-dom";
 import { List } from "../components/List";
 import { SliderTop } from "../components/SliderTop";
 
 import "../styles/global.css";
 
 export function Home() {
-  const location = useLocation()
+  const location = useLocation();
   useEffect(() => {
-    console.log("Navegação alterada: ", location.pathname)
-  }, [location])
+    console.log("Navegação alterada: ", location.pathname);
+  }, [location]);
 
-  const isRouteHome = useMatch("/fun")
+  const isRouteHome = useMatch("/fun");
   return (
     <div className="w-100% md:w-[calc(100%)] h-screen md:p-8 py-6 px-4">
       <div className="header mb-12">
         <nav className="flex justify-between w-full items-center">
           <ul className="text-white flex gap-6">
-            <NavLink to={`/fun`} activeClassName={'active'}>Início</NavLink>
-            <NavLink to={"/fun/series"}  activeClassName={'active'}>Séries</NavLink>
-            <NavLink to={"/fun/movies"}  activeClassName={'active'}>Filmes</NavLink>
-            <NavLink to={"/fun/premieres"}  activeClassName={'active'}>Lançamentos</NavLink>
-            <NavLink to={"/fun/top"} activeClassName={'active'}>Bombando</NavLink>
+            <NavLink to={`/fun`} activeClassName={"active"}>
+              {" "}
+              <Popcorn size={24} />
+            </NavLink>
+            <NavLink to={"/fun/series"} activeClassName={"active"}>
+              Séries
+            </NavLink>
+            <NavLink to={"/fun/movies"} activeClassName={"active"}>
+              Filmes
+            </NavLink>
+            <NavLink to={"/fun/premieres"} activeClassName={"active"}>
+              Lançamentos
+            </NavLink>
+            <NavLink to={"/fun/top"} activeClassName={"active"}>
+              Bombando
+            </NavLink>
           </ul>
         </nav>
       </div>
-      {
-        isRouteHome ? (<SliderTop/>) : (<List type={location.pathname}/>)
-      }
+      {isRouteHome ? (
+        <div>
+          <SliderTop />
+          <List type={location.pathname} />
+        </div>
+      ) : (
+        <List type={location.pathname} />
+      )}
     </div>
   );
 }
