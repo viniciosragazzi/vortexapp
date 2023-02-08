7dee0fb841f5432894512e4a001596ebb6936fe4
viniciosragazzi <viniciosragazzzi@gmail.com>
2023-02-07 21:14:17 -0300

    Algumas Reqs adicionadas

--- src/pages/FunChildrenPage.jsx
+++ src/pages/FunChildrenPage.jsx
@@ -19,15 +19,12 @@ export default function FunChildrenPage({ acaoTv }) {
   const [acaoMovie, setAcaoMovie] = useState([]);
 
   const [loading, setLoading] = useState(true);
-  const [loadingTop, setLoadingTop] = useState(true);
-
   useEffect(() => {
     async function fetchData() {
-      setLoadingTop(true);
       setLoading(true);
       const nowPlayingData = await nowPlaying(typeConverted, 1);
       setPlayingNow(nowPlayingData);
-      setLoadingTop(false);
+
       const animationItens = await getIdOfGenresAndReturnDatas(
         "Animação",
         typeConverted,
@@ -48,29 +45,21 @@ export default function FunChildrenPage({ acaoTv }) {
 
   return (
     <div className="w-full">
-      <>
-        {loadingTop ? (
-          <div className="w-full h-screen flex justify-center items-center pt-10 text-4xl text-white">
-            <CircularProgress />
-          </div>
-        ) : (
-          <SliderTop itens={playingNow} loading={loading} />
-        )}
+      {loading ? (
+        <div className="w-full h-screen flex justify-center items-center pt-10 text-4xl text-white">
+          <CircularProgress />
+        </div>
+      ) : (
+        <SliderTop itens={playingNow} loading={loading} />
+      )}
 
-        {loading ? (
-          <div className="w-full h-screen flex justify-center items-center pt-10 text-4xl text-white">
-            <CircularProgress />
-          </div>
-        ) : (
-          <List
-            type={location.pathname}
-            animacao={animation}
-            popular={popular}
-            acaoTv={acaoTv}
-            acao={acaoMovie}
-          />
-        )}
-      </>
+      <List
+        type={location.pathname}
+        animacao={animation}
+        popular={popular}
+        acaoTv={acaoTv}
+        acao={acaoMovie}
+      />
     </div>
   );
 }
